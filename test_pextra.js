6c3fb26d83690f656325b185b41752c8b74f69ba
truongthao131204 <truongdangvithao1312@gmail.com>
2026-04-17 09:51:58 +0700

    chore: clean up scratch test files

--- test_pextra.js
+++
@@ -1,5 +0,0 @@
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin());
-
-console.log('Puppeteer extra loaded successfully');
